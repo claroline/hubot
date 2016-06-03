1a4469548ce335f62bc2a1428198c0b11ee808d3
Stéphane Klein <stephaneklein221@gmail.com>
2016-06-03 18:48:28 +0200

    Add doc and remove escape character

--- scripts/self-deploy.js
+++ scripts/self-deploy.js
@@ -2,13 +2,13 @@
 //   Refreshes the sources of the bot and restarts it.
 //
 // Commands:
-//   hubot self-deploy - re-deploy the bot with the latest sources
+//   hubot self-deploy [branch] - re-deploy the bot from a github branch (defaults to master)
 
 const makeExec = require('./../lib/exec-promise')
 const appDir = `${__dirname}/..`
 
 module.exports = robot => {
-  robot.respond(/self\-deploy( (.+))?/, res => {
+  robot.respond(/self-deploy( (.+))?/, res => {
     const branch = res.match[2] || 'master'
     const exec = makeExec(res.reply.bind(res))
 
