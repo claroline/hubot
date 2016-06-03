6b45dedfe516941da22a237c50489ca7bc0bc82e
Stéphane Klein <stephaneklein221@gmail.com>
2016-06-03 18:31:34 +0200

    Enable deployment of specific branch

---
+++ scripts/self-deploy.js
@@ -0,0 +1,26 @@
+// Description:
+//   Refreshes the sources of the bot and restarts it.
+//
+// Commands:
+//   hubot self-deploy - re-deploy the bot with the latest sources
+
+const makeExec = require('./../lib/exec-promise')
+const appDir = `${__dirname}/..`
+
+module.exports = robot => {
+  robot.respond(/self\-deploy( (.+))?/, res => {
+    const branch = res.match[2] || 'master'
+    const exec = makeExec(res.reply.bind(res))
+
+    res.reply(`Re-deploying myself from ${branch} branch...`)
+
+    exec(`cd ${appDir}`)
+      .then(() => exec('git fetch'))
+      .then(() => exec(`git checkout ${branch}`))
+      .then(() => exec('npm install))
+      .then(() => exec('echo "Going to restart now..."'))
+      .then(() => exec('sleep 1'))
+      .then(() => exec('supervisorctl restart clarobot'))
+      .catch(() => res.reply('Self deployment failed'))
+  })
+}
