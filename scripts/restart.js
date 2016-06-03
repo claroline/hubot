20c92bba071b95aeabd1678364a4072182570ce6
Stéphane Klein <stephaneklein221@gmail.com>
2016-06-03 16:43:53 +0200

    Add auto-refresh script

---
+++ scripts/restart.js
@@ -0,0 +1,20 @@
+// Description:
+//   Refreshes the sources of the bot and restarts it.
+//
+// Commands:
+//   hubot refresh - re-deploy the bot with the latest sources
+
+const makeExec = require('./../exec-promise')
+const exec = makeExec(console.log.bind(console))
+const appDir = `${__dirname}/..`
+
+module.exports = robot => {
+  robot.respond(/refresh/i, res => {
+    res.reply('Re-deploying myself...')
+    exec(`cd ${appDir}`)
+      .then(() => exec('git checkout master'))
+      .then(() => exec('git pull'))
+      .then(() => exec('npm i'))
+      .then(() => exec('supervisorctl restart clarobot'))
+  })
+}
